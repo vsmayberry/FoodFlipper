3d545990cf97c32f100b398812e7010749995b35
Joshua Solomon <jjsolomon@email.arizona.edu>
2015-04-10 00:38:07 -0700

    Worked on the game activity UI mostly, other minor changes

---
+++ app/src/main/java/arizona/edu/foodflipper/GameActivity.java
@@ -0,0 +1,72 @@
+package arizona.edu.foodflipper;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class GameActivity extends ActionBarActivity {
+
+    static final int LOGIN_USER = 1;
+    public static final String PREFS_NAME = "MyPrefsFile";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean isLoggedIn = settings.getBoolean("isLoggedIn", false);
+        if (!isLoggedIn) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivityForResult(intent, LOGIN_USER);
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        boolean isLoggedIn = settings.getBoolean("isLoggedIn", false);
+        if (requestCode == LOGIN_USER) {
+            if (isLoggedIn) {
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivityForResult(intent, LOGIN_USER);
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
